d1143d03c9b4a27148352f18f84d08911ccd030c
William Mbotta <sergembotta@mailoo.org>
2019-10-10 02:50:33 +0200

    event on square

--- src/board.rs
+++ src/board.rs
@@ -1,84 +1,86 @@
 extern crate js_sys;
-use web_sys::console;
 use wasm_bindgen::prelude::*;
+use web_sys::console;
 
 static NEIGBOURHOOD_OFFSETS: &'static [(i32, i32)] = &[
-    (-1, -1), (0, -1), (1, -1),
-    (-1,  0),          (1,  0),
-    (-1,  1), (0,  1), (1,  1),
+    (-1, -1),
+    (0, -1),
+    (1, -1),
+    (-1, 0),
+    (1, 0),
+    (-1, 1),
+    (0, 1),
+    (1, 1),
 ];
 
 pub fn annotate(field: Vec<String>) -> Vec<String> {
     let height = field.len() as i32;
-    (0..height).map(|y| {
-        let width = field[y as usize].len() as i32;
-        (0..width).map(|x| {
-            if field[y as usize].as_bytes()[x as usize] == b'*' {
-                '*'
-            } else {
-                match NEIGBOURHOOD_OFFSETS.iter()
-                    .map(|&(ox, oy)| (x + ox, y + oy))
-                    .filter(|&(x, y)| (0 <= x && x < width) && (0 <= y && y < height))
-                    .filter(|&(x, y)| field[y as usize].as_bytes()[x as usize] == b'*')
-                    .count() {
-                        0 => ' ',
-                        n => (n as u8 + '0' as u8) as char
+    (0..height)
+        .map(|y| {
+            let width = field[y as usize].len() as i32;
+            (0..width)
+                .map(|x| {
+                    if field[y as usize].as_bytes()[x as usize] == b'*' {
+                        '*'
+                    } else {
+                        match NEIGBOURHOOD_OFFSETS
+                            .iter()
+                            .map(|&(ox, oy)| (x + ox, y + oy))
+                            .filter(|&(x, y)| (0 <= x && x < width) && (0 <= y && y < height))
+                            .filter(|&(x, y)| field[y as usize].as_bytes()[x as usize] == b'*')
+                            .count()
+                        {
+                            0 => ' ',
+                            n => (n as u8 + '0' as u8) as char,
+                        }
                     }
-            }
-        }).collect()
-    }).collect()
+                })
+                .collect()
+        })
+        .collect()
 }
-
 
 pub enum LEVEL {
     EASY,
     MEDIUM,
-    HARD
+    HARD,
 }
 
 pub fn generate(level: LEVEL) -> Vec<String> {
     let size = match level {
-        LEVEL::EASY =>  9,
+        LEVEL::EASY => 9,
         LEVEL::MEDIUM => 48,
-        LEVEL::HARD => 98
+        LEVEL::HARD => 98,
     };
 
-    let mut mines_nbr : usize = size + 1;
-    let mut indices : Vec<usize> = (0..mines_nbr * mines_nbr).map(|indice| indice).collect();
+    let mut mines_nbr: usize = size + 1;
+    let mut indices: Vec<usize> = (0..mines_nbr * mines_nbr).map(|indice| indice).collect();
     let mut mine_locations = vec![];
 
-
     for _ in 0..mines_nbr {
-         let index = js_sys::Math::floor(js_sys::Math::random() * indices.len() as f64) as usize;
-         mine_locations.push(index);
-         indices.remove(index);
-         mines_nbr -= 1;
+        let index = js_sys::Math::floor(js_sys::Math::random() * indices.len() as f64) as usize;
+        mine_locations.push(index);
+        indices.remove(index);
+        mines_nbr -= 1;
     }
     mine_locations.sort();
-    // console::log_1(&JsValue::from_str(&"in generated function"));
-    // console::log_1(&JsValue::from(mine_locations.len() as u32));
-    // console::log_1(&JsValue::from_str(&"in generated function"));
 
-    
+
     let mut num = 0;
-    let a: Vec<String> = (0..size).enumerate().map(|_| {
-        (0..size).map(|_| {
-            if mine_locations.iter().any(|index| index == &num) {
-                console::log_1(&JsValue::from_str("true"));
-                num += 1;
-                "*"
-            } else {
-                console::log_1(&JsValue::from_str("false"));
-                num += 1;
-                " "
-            }
-        }).collect()
-    }).collect();
-
-    for (i, line) in a.iter().enumerate() {
-        console::log_1(&JsValue::from(line.len() as u32));
-        console::log_1(&JsValue::from_str(&"line.length"));
-        console::log_1(&JsValue::from_str(line));
-    }
-    a
+    (0..size)
+        .enumerate()
+        .map(|_| {
+            (0..size)
+                .map(|_| {
+                    if mine_locations.iter().any(|index| index == &num) {
+                        num += 1;
+                        "*"
+                    } else {
+                        num += 1;
+                        " "
+                    }
+                })
+                .collect()
+        })
+        .collect()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 mod board;
 
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 use web_sys::console;
+
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
 //
@@ -20,38 +22,45 @@ pub fn main_js() -> Result<(), JsValue> {
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
 
-    // Your code goes here!
-  //  console::log_1(&JsValue::from_str("Hello world!"));
     let board = board::generate(board::LEVEL::EASY);
-    //console::log_1(&JsValue::from(board.len() as u32));
-    let boardDom = document.query_selector("#board").expect("not fail").unwrap();
-   // console::log_1(&JsValue::from_str("taille"));
-    //console::log_1(&JsValue::from(board::annotate(board).len() as u32));
-   // console::log_1(&JsValue::from_str("taille"));
+    let boardDom = document
+        .query_selector("#board")
+        .expect("not fail")
+        .unwrap();
+
     let final_board = board::annotate(board);
     for (y, row) in final_board.iter().enumerate() {
-        console::log_1(&JsValue::from_str(&"BEGINNNNNNNNNNNNNNNNNNNNNNNNNNNN row"));
-        console::log_1(&JsValue::from(row));
-      
-     //   console::log_1(&JsValue::from_str(row));
-        row.chars().enumerate().for_each(|(x,_)| {
-            let div = document.create_element("button").expect("no global `window` exists");
-            console::log_1(&JsValue::from_str(&"x begin"));
-            console::log_1(&JsValue::from(x as u32));
-            console::log_1(&JsValue::from_str(&"x end"));
-            div.set_class_name("square");
-            div.set_attribute("data-x", &format!("{}", x)).expect("no global `window` exists");
-            div.set_attribute("data-y", &format!("{}", y)).expect("no global `window` exists");
-            boardDom.append_child(&div).expect("not fail");
-        });
-                  console::log_1(&JsValue::from_str(&"ENNNNNNNNNNNNNDDDDDDDDDDD! row"));
-    }
 
-        //     console::log_1(&JsValue::from(i as u32));
-        //     console::log_1(&JsValue::from_str(&line));
-        //     console::log_1(&JsValue::from_str(&line));
-    //console::log_1(&JsValue::from(&board));
-    //console::log_1(&JsValue::from_str(&board::annotate(board)));
+        row.chars().enumerate().for_each(|(x, value)| {
+            let div = document
+                .create_element("button")
+                .expect("no global `window` exists");
+            div.set_class_name("square");
+            div.set_attribute("data-x", &format!("{}", x))
+                .expect("no global `window` exists");
+            div.set_attribute("data-y", &format!("{}", y))
+                .expect("no global `window` exists");
+            boardDom.append_child(&div).expect("not fail");
+            let cloned_div = div.clone();
+            let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+                console::log_1(&JsValue::from(x as u32));
+                console::log_1(&JsValue::from(y as u32));
+                console::log_1(&JsValue::from_str(&value.to_string()));
+                if value == '*' {
+                    div.set_class_name("square bombed trigger");
+                } else if value.is_digit(10) {
+                     div.set_class_name(&format!("square cleared bomb-{}", value));
+                     div.set_inner_html(&value.to_string());
+                } else {
+                    div.set_class_name("square cleared");
+                }
+            }) as Box<dyn FnMut(_)>);
+
+            cloned_div.add_event_listener_with_callback("click", closure.as_ref().unchecked_ref())
+                .expect("error");
+            closure.forget();
+        });
+    }
 
     Ok(())
 }
