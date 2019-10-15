11a9b3e0091f51952f0393871b8b4aa16ad45831
William Mbotta <sergembotta@mailoo.org>
2019-10-16 00:35:35 +0200

    disabled after bomb

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod board;
 
+use std::sync::{Arc, Mutex};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::console;
@@ -31,7 +32,9 @@ pub fn main_js() -> Result<(), JsValue> {
         init();
     }) as Box<dyn FnMut(_)>);
 
-    restart_button.add_event_listener_with_callback("click", closure3.as_ref().unchecked_ref());
+    restart_button
+        .add_event_listener_with_callback("click", closure3.as_ref().unchecked_ref())
+        .expect("error");
 
     fn init() {
         let board = board::generate(board::LEVEL::EASY);
@@ -43,6 +46,8 @@ pub fn main_js() -> Result<(), JsValue> {
             .unwrap();
         let final_board = board::annotate(board);
         let width = final_board.len();
+        let global_state: Arc<Mutex<Vec<web_sys::Element>>> = Arc::new(Mutex::new(vec![]));
+
         for (y, row) in final_board.iter().enumerate() {
             row.chars().enumerate().for_each(|(x, value)| {
                 let div = document
@@ -59,11 +64,14 @@ pub fn main_js() -> Result<(), JsValue> {
                     .expect("no global `window` exists");
                 div.set_attribute("value", &format!("{}", value))
                     .expect("no global `window` exists");
+                let ref mut merrr = *global_state.lock().unwrap();
+                merrr.push(div.clone());
                 boardDom.append_child(&div).expect("not fail");
+                let clone_global = global_state.clone();
                 let cloned_div = div.clone();
                 let cloned_div1 = div.clone();
-
                 let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+                    // let ref mut global_state = *global_state.lock().unwrap();
                     console::log_1(&JsValue::from(x as u32));
                     console::log_1(&JsValue::from(y as u32));
                     console::log_1(&JsValue::from_str(&value.to_string()));
@@ -72,6 +80,11 @@ pub fn main_js() -> Result<(), JsValue> {
                         div.set_class_name("square bombed trigger");
                         div.set_attribute("status", "bombed")
                             .expect("no global `window` exists");
+                        //  web_sys::window().unwrap().document().unwrap().query_selector_all("#board button").expect("nodelist");
+                        let ref mut global_state = *clone_global.lock().unwrap();
+                        global_state.iter().for_each(|node| {
+                            node.set_attribute("disabled", "false");
+                        });
                     } else if value.is_digit(10) {
                         div.set_attribute("status", "clicked")
                             .expect("no global `window` exists");
@@ -101,14 +114,14 @@ pub fn main_js() -> Result<(), JsValue> {
                         cloned_div1
                             .set_attribute("status", "flagged")
                             .expect("no global `window` exists");
-                        cloned_div1.set_class_name("square flagged");    
+                        cloned_div1.set_class_name("square flagged");
                         score -= 1;
                         score_node.set_inner_html(&format!("00{}", score));
                     } else {
                         cloned_div1
                             .set_attribute("status", "")
                             .expect("no global `window` exists");
-                        cloned_div1.set_class_name("square");      
+                        cloned_div1.set_class_name("square");
                         score += 1;
                         score_node.set_inner_html(&format!("00{}", score));
                     }
